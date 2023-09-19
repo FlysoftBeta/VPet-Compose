9c5fc2c636d73965ba9c5683a1b23889bf396dd9
Flysoft <flysoftbeta@outlook.com>
2023-09-19 21:26:33 +0800

    chore(ui/main): Code cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -12,7 +11,6 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import ui.theme.AppTheme
 
-
 @Composable
 @Preview
 fun WindowScope.App() {
@@ -20,9 +18,6 @@ fun WindowScope.App() {
         Pet()
     }
 }
-
-@OptIn(ExperimentalComposeUiApi::class)
-
 
 fun main() = application {
     ManagersProvider {

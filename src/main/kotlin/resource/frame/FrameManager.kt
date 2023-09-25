4734a9413bbfff5939d2917aaf67b86a5b13987c
Flysoft <flysoftbeta@outlook.com>
2023-09-25 23:41:50 +0800

    feat(resource/frame/manager): Resize image to reduce memory usage

--- src/main/kotlin/resource/frame/FrameManager.kt
+++ src/main/kotlin/resource/frame/FrameManager.kt
@@ -4,18 +4,27 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Image
+import org.jetbrains.skia.ImageInfo
+import org.jetbrains.skia.SamplingMode
 import utils.FixedSizeLinkedHashMap
 
 class FrameManager {
-    private val loadedFrames = FixedSizeLinkedHashMap<Frame, ImageBitmap>(160)
+    private val loadedFrames = FixedSizeLinkedHashMap<Frame, ImageBitmap>(40)
 
     suspend fun loadFrames(frames: List<Frame>): List<ImageBitmap> {
         return frames.map { frame ->
             loadedFrames.getOrPut(frame) {
-                Image.makeFromEncoded(withContext(Dispatchers.IO) {
+                val image = Image.makeFromEncoded(withContext(Dispatchers.IO) {
                     frame.file.readBytes()
-                }).toComposeImageBitmap()
+                })
+                val bitmap = Bitmap()
+                bitmap.setImageInfo(ImageInfo(500, 500, image.colorType, image.alphaType))
+                bitmap.allocPixels()
+                val pixmap = bitmap.peekPixels()!!
+                image.scalePixels(pixmap, SamplingMode.LINEAR, false)
+                Image.makeFromPixmap(pixmap).toComposeImageBitmap()
             }
         }
     }
